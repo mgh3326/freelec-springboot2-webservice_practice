54905712e465bbd0f7e8b652d63f774221180b71
kwanghyunMoon <mgh3326@naver.com>
2019-12-23 21:04:28 +0900

    Initial commit

---
+++ src/main/java/me/khmmon/freelecspringboot2webservice_practice/FreelecSpringboot2WebservicePracticeApplication.java
@@ -0,0 +1,13 @@
+package me.khmmon.freelecspringboot2webservice_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelecSpringboot2WebservicePracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FreelecSpringboot2WebservicePracticeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/me/khmmon/freelecspringboot2webservice_practice/FreelecSpringboot2WebservicePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.khmmon.freelecspringboot2webservice_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelecSpringboot2WebservicePracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
